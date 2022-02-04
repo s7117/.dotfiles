44fbf8762a17666e4558e9539ba436659a84ad79
Peyton Chandarana <peytonchandarana@gmail.com>
2022-02-03 19:33:46 -0500

    Added easy install commands.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 mkdir ~/.cli_tools
 
-echo "# CLI Tools" >> ~/.zshrc
+# Oh-My-Posh Install
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
 
+echo 'eval "$(oh-my-posh --init --shell zsh --config ~/.dotfiles/s7117.omp.json)"' >> ~/.zshrc
+echo 'source ~/.dotfiles/.zshrc_custom' >> ~/.zshrc
+
+echo "# CLI Tools" >> ~/.zshrc
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.cli_tools/zsh-autosuggestions
 echo "source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
