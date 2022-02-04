0cdc9abc2360331d19d68c8ad008c691333c8aad
Peyton Chandarana <peytonchandarana@gmail.com>
2022-02-03 19:12:42 -0500

    corrected clicolors to clicolor

---
+++ install_cli_tools.sh
@@ -0,0 +1,11 @@
+mkdir ~/.cli_tools
+
+echo "# CLI Tools" >> ~/.zshrc
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.cli_tools/zsh-autosuggestions
+echo "source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.cli_tools/zsh-syntax-highlighting
+echo "source ~/.cli_tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
