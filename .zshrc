afacc57a6e3b2b6eec2235b00c69b9887cf9abd1
Peyton Chandarana <peytonchandarana@gmail.com>
2022-02-04 16:44:09 -0500

    Updated zshrc file generation to have history and added completions.

--- .zshrc
+++ .zshrc
@@ -1,22 +1,30 @@
-export PATH=$PATH:~/.oh-my-posh/bin
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt appendhistory
+
 eval "$(oh-my-posh --init --shell zsh --config ~/.dotfiles/s7117.omp.json)"
 source ~/.dotfiles/.zshrc_custom
-# CLI Tools
-source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.cli_tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/homes/psc/.miniforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/psc/.miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/homes/psc/.miniforge/etc/profile.d/conda.sh" ]; then
-        . "/homes/psc/.miniforge/etc/profile.d/conda.sh"
+    if [ -f "/home/psc/.miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/home/psc/.miniforge3/etc/profile.d/conda.sh"
     else
-        export PATH="/homes/psc/.miniforge/bin:$PATH"
+        export PATH="/home/psc/.miniforge3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
 
+# Exports
+export fpath=(/home/psc/.cli_tools/zsh-completions/src $fpath)
+
+# CLI Tools
+source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.cli_tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.cli_tools/zsh-completions/zsh-completions.plugin.zsh
--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@ mkdir ~/.cli_tools
 mkdir ~/.oh-my-posh
 mkdir ~/.oh-my-posh/bin
 
+# Zsh Settings
+echo "HISTFILE=~/.zsh_history" >> .zshrc
+echo "HISTSIZE=10000" >> .zshrc
+echo "SAVEHIST=10000" >> .zshrc
+echo "setopt appendhistory" >> .zshrc
+
 # Oh-My-Posh Install
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O ~/.oh-my-posh/bin/oh-my-posh
 chmod +x ~/.oh-my-posh/bin/oh-my-posh
@@ -11,6 +17,7 @@ echo 'eval "$(oh-my-posh --init --shell zsh --config ~/.dotfiles/s7117.omp.json)
 echo 'source ~/.dotfiles/.zshrc_custom' >> ~/.zshrc
 
 echo "# CLI Tools" >> ~/.zshrc
+
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.cli_tools/zsh-autosuggestions
 echo "source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
@@ -18,3 +25,9 @@ echo "source ~/.cli_tools/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zsh
 # Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.cli_tools/zsh-syntax-highlighting
 echo "source ~/.cli_tools/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+# Install zsh-completions
+git clone https://github.com/zsh-users/zsh-completions.git ~/.cli_tools/zsh-completions
+export fpath=(~/.cli_tools/zsh-completions/src $fpath)
+echo "source ~/.cli_tools/zsh-completions/zsh-completions.plugin.zsh" >> ~/.zshrc
+rm -f ~/.zcompdump; compinit
