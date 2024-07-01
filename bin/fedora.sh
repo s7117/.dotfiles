c5dc4ae4b59634f7054a4899d07db20b19084fe9
Peyton Chandarana <peytonchandarana@gmail.com>
2024-07-01 02:21:50 -0400

    Updated Fedora script.

--- bin/fedora.sh
+++ bin/fedora.sh
@@ -26,6 +26,33 @@ mkdir -p ~/.ssh
 cp ./etc/config ~/.ssh
 cp ./etc/vimrc ~/.vimrc
 ################################################################################
+# Install
+sudo dnf remove docker \
+	docker-client \
+	docker-client-latest \
+	docker-common \
+	docker-latest \
+	docker-latest-logrotate \
+	docker-logrotate \
+	docker-selinux \
+	docker-engine-selinux \
+	docker-engine
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+	--add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin
+
+echo "LOG --> Check that the following GPG matches:"
+echo "LOG --> 060A 61C5 1B55 8A7F 742B 77AA C52F EB6B 621E 9F35"
+
+
+################################################################################
 # Services
 sudo systemctl enable ufw
 sudo systemctl start ufw
