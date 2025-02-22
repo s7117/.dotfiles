bc21fef800315d42936f17d293e29724e5b58ed8
Peyton Chandarana <peytonchandarana@gmail.com>
2025-02-22 00:52:26 -0500

    Added -y to docker install.

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -65,7 +65,7 @@ echo \
 sudo apt-get update -y
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Add user to docker group
 sudo usermod -aG docker $USER
